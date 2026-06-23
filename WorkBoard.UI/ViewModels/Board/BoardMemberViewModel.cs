eeba0e3897742028be36d50e987f24eced02cb4c
Tanya Tverdun <tanyatverdun@gmail.com>
2026-06-24 02:05:38 +0300

    feat: create UI for board members CRUD

---
+++ WorkBoard.UI/ViewModels/Board/BoardMemberViewModel.cs
@@ -0,0 +1,15 @@
+﻿using WorkBoard.Services.Abstraction.DTOs;
+
+namespace WorkBoard.UI.ViewModels.Board;
+
+public class BoardMemberViewModel
+{
+    public BoardMemberDto Dto { get; set; }
+
+    public bool IsConfirmingDelete { get; set; }
+
+    public BoardMemberViewModel(BoardMemberDto dto)
+    {
+        Dto = dto;
+    }
+}
