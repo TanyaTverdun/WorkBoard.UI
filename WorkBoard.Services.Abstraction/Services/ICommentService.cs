8e6fc721c7b2e2c1f53d739a4eb2c023f7705e84
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-07-08 12:22:47 +0300

    feat: create CommentService and GetCommentsByCardAsync method

---
+++ WorkBoard.Services.Abstraction/Services/ICommentService.cs
@@ -0,0 +1,10 @@
+﻿using WorkBoard.Services.Abstraction.DTOs;
+
+namespace WorkBoard.Services.Abstraction.Services;
+
+public interface ICommentService
+{
+    Task<IReadOnlyList<CommentDto>> GetCommentsByCardAsync(
+        Guid cardId,
+        CancellationToken cancellationToken = default);
+}
---
+++ WorkBoard.Services/Servises/Comment/ICommentApi.cs
@@ -0,0 +1,12 @@
+﻿using Refit;
+using WorkBoard.Services.Abstraction.DTOs;
+
+namespace WorkBoard.Services.Servises.Comment;
+
+internal interface ICommentApi
+{
+    [Get("/api/cards/{cardId}/comments")]
+    Task<IReadOnlyList<CommentDto>> GetCommentsByCardAsync(
+        Guid cardId,
+        CancellationToken cancellationToken = default);
+}
