ca5468f4a3b70045d28c1badf90b00c5d7d701d3
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-07-21 15:53:25 +0300

    feat: add user profile popover

--- WorkBoard.UI/Components/Sidebar.razor.cs
+++ WorkBoard.UI/Components/Sidebar.razor.cs
@@ -1,33 +1,35 @@
 ﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Authorization;
 using WorkBoard.Domain.Constants;
+using WorkBoard.Services.Abstraction.StateProviders;
 
 namespace WorkBoard.UI.Components;
 
 public partial class Sidebar
 {
-    [CascadingParameter]
-    private Task<AuthenticationState>? AuthenticationStateTask { get; set; }
+    [Inject]
+    private ICurrentUserProvider CurrentUserProvider { get; set; } = default!;
 
     protected string Username { get; private set; } = UiConstants.Auth.LoadingText;
     protected string Initials { get; private set; } = string.Empty;
-
+    protected string UserEmail { get; private set; } = string.Empty;
     protected string SearchQuery { get; set; } = string.Empty;
 
     protected override async Task OnInitializedAsync()
     {
-        if (AuthenticationStateTask is not null)
+        var fullName = await CurrentUserProvider.GetFullNameAsync();
+        var email = await CurrentUserProvider.GetEmailAsync();
+
+        if (!string.IsNullOrEmpty(fullName))
         {
-            var authState = await AuthenticationStateTask;
-            var user = authState.User;
-
-            if (user.Identity is { IsAuthenticated: true })
-            {
-                Username = user.Identity.Name ?? UiConstants.Auth.DefaultUsername;
-
-                SetInitials(Username);
-            }
+            Username = fullName;
+            SetInitials(Username);
         }
+        else
+        {
+            Username = UiConstants.Auth.DefaultUsername;
+        }
+
+        UserEmail = email ?? string.Empty;
     }
 
     private void SetInitials(string name)
@@ -50,5 +52,26 @@ public partial class Sidebar
                 ? parts[0][..2].ToUpper()
                 : parts[0][0].ToString().ToUpper();
         }
+    }
+
+    private bool _isProfileMenuOpen = false;
+
+    private void ToggleProfileMenu()
+    {
+        _isProfileMenuOpen = !_isProfileMenuOpen;
+    }
+
+    private void GoToProfile()
+    {
+        _isProfileMenuOpen = false;
+        // Твоя логіка переходу на сторінку профілю
+        // NavigationManager.NavigateTo("/profile");
+    }
+
+    private async Task SignOut()
+    {
+        _isProfileMenuOpen = false;
+        // Твоя логіка виходу (наприклад, очищення токену Azure AD)
+        // await AuthService.LogoutAsync();
     }
 }
