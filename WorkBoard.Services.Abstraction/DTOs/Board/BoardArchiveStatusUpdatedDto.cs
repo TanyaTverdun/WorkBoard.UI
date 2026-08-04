d41538570c16a1f93d0d17ad7baeee112f21a1ad
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-08-04 17:59:58 +0300

    feat: add OnBoardArchiveStatusUpdated event to BoardHubService

---
+++ WorkBoard.Services.Abstraction/DTOs/Board/BoardArchiveStatusUpdatedDto.cs
@@ -0,0 +1,7 @@
+﻿namespace WorkBoard.Services.Abstraction.DTOs.Board;
+
+public class BoardArchiveStatusUpdatedDto
+{
+    public Guid BoardId { get; set; }
+    public bool IsArchived { get; set; }
+}
