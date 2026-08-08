04e348e3d1a83f6fe998fae364b234ebf6a261da
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-08-08 16:59:42 +0300

    feat: add ChatMessageDto, ChatResponseDto and ChatRequestDto

---
+++ WorkBoard.Services.Abstraction/DTOs/Chat/ChatResponseDto.cs
@@ -0,0 +1,6 @@
+﻿namespace WorkBoard.Services.Abstraction.DTOs.Chat;
+
+public class ChatResponseDto
+{
+    public string Answer { get; set; } = string.Empty;
+}
---
+++ WorkBoard.Services.Abstraction/Requests/Chat/ChatRequestDto.cs
@@ -0,0 +1,8 @@
+﻿using WorkBoard.Services.Abstraction.DTOs.Chat;
+
+namespace WorkBoard.Services.Abstraction.Requests.Chat;
+
+public class ChatRequestDto
+{
+    public List<ChatMessageDto> Messages { get; set; } = new();
+}
